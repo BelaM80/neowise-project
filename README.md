5a4d0ff1417783a890dce2ca7c8a9c2379680aed
BelaM80 <71004683+BelaM80@users.noreply.github.com>
2020-11-24 10:19:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# neowise-project
---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import Page from "./components/pages/Page.js";
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <Page />
+      <a href="/storybook">To Storybook</a>
+    </div>
+  );
+}
+
+export default App;
