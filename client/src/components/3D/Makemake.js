78402233f67491218ccbc24fab9eaf49ab950097
BelaM80 <mueller.bela@gmx.de>
2020-12-03 17:51:31 +0100

    Add rotation to Makemake

--- client/src/components/3D/Makemake.js
+++ client/src/components/3D/Makemake.js
@@ -1,15 +1,27 @@
 import React, { useRef } from "react";
 import { useGLTFLoader } from "drei";
 import makemake from "../../assets/Makemake_1_1430.glb";
+import { useFrame } from "react-three-fiber";
+import PropTypes from "prop-types";
+import { animated } from "react-spring-three";
 
-const MakeMake = () => {
+const MakeMake = ({ position, scale }) => {
   const mesh = useRef(null);
   const gltf = useGLTFLoader(makemake, true);
+  useFrame(() => {
+    mesh.current.rotation.y += 0.01;
+  });
   return (
-    <mesh position={[0, 1, 0]} castShadow>
+    <animated.mesh position={position} castShadow speed={0.3} scale={scale}>
       <primitive object={gltf.scene} dispose={null} ref={mesh} />
-    </mesh>
+    </animated.mesh>
   );
+};
+
+MakeMake.propTypes = {
+  speed: PropTypes.number.isRequired,
+  position: PropTypes.arrayOf(PropTypes.number).isRequired,
+  scale: PropTypes.arrayOf(PropTypes.number).isRequired,
 };
 
 export default MakeMake;
--- client/src/components/3D/Makemake.stories.js
+++ client/src/components/3D/Makemake.stories.js
@@ -12,7 +12,7 @@ const Template = () => (
   <Canvas colorManagement camera={{ position: [-5, 2, 10], fov: 70 }}>
     <directionalLight position={[0, 10, 0]} intensity={0.5} />
     <Suspense fallback={null}>
-      <mesh scale={[0.01, 0.01, 0.01]} position={[0, 0, 0]}>
+      <mesh scale={[0.01, 0.01, 0.01]} position={[0, 0, 0]} speed={0.3}>
         <MakeMake />
       </mesh>
     </Suspense>
