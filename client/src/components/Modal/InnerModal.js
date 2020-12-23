9a1e23139d7c6fcd508e8797aaf5d43c036962f7
BelaM80 <mueller.bela@gmx.de>
2020-12-23 11:08:14 +0100

    Add macro to styled-components

--- client/src/components/Modal/InnerModal.js
+++ client/src/components/Modal/InnerModal.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styled from "styled-components";
+import styled from "styled-components/macro";
 
 const Frame = styled.div`
   /* border: 1px solid lightgrey; */
